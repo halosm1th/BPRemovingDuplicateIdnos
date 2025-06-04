a8f89b2b918e0d2105408255c6cbd42a4609857b
halosm1th <16440148+halosm1th@users.noreply.github.com>
2025-06-03 23:49:22 -0500

    Started building the git repo for the program to find matching BP's and fix them.

---
+++ BPRemovingDuplicateIdnos/BPDataEntry.cs
@@ -0,0 +1,170 @@
+﻿using System.Text;
+
+namespace DefaultNamespace;
+
+public class BPDataEntry
+{
+    private string? _bpNum = null;
+    private string? _cr = null;
+    private string? _index = null;
+    private string? _indexBis = null;
+    private string? _internet = null;
+    private string? _name = null;
+    private string? _no = null;
+    private string? _publication = null;
+    private string? _resume = null;
+    private string? _sbandseg = null;
+    private string? _title = null;
+
+    private string? annee = null;
+
+    //At a minimum all entries must have one number
+    public BPDataEntry(string? number, Logger logger)
+    {
+        BPNumber = number;
+        this.logger = logger;
+    }
+
+    protected Logger logger { get; }
+
+    public string? Index
+    {
+        get => _index;
+        set => _index = ReplaceInvalidText(value);
+    }
+
+    public bool HasIndex => _index != null;
+
+    public string? IndexBis
+    {
+        get => _indexBis;
+        set => _indexBis = ReplaceInvalidText(value);
+    }
+
+    public bool HasIndexBis => _indexBis != null;
+
+    public string? Title
+    {
+        get => _title;
+        set => _title = ReplaceInvalidText(value);
+    }
+
+    public bool HasTitle => _title != null;
+
+    public string? Publication
+    {
+        get => _publication;
+        set => _publication = ReplaceInvalidText(value);
+    }
+
+    public bool HasPublication => _publication != null;
+
+    public string? Internet
+    {
+        get => _internet;
+        set => _internet = ReplaceInvalidText(value);
+    }
+
+    public bool HasInternet => _internet != null;
+
+    public string? SBandSEG
+    {
+        get => _sbandseg;
+        set => _sbandseg = ReplaceInvalidText(value);
+    }
+
+    public bool HasSBandSEG => _sbandseg != null;
+
+    public string? No
+    {
+        get => _no;
+        set => _no = ReplaceInvalidText(value);
+    }
+
+    public bool HasNo => _no != null;
+
+    public string? Annee
+    {
+        get => annee;
+        set => annee = ReplaceInvalidText(value);
+    }
+
+    public bool HasAnnee => annee != null;
+
+    public string? Resume
+    {
+        get => _resume;
+        set => _resume = ReplaceInvalidText(value);
+    }
+
+    public bool HasResume => _resume != null;
+
+    public string? CR
+    {
+        get => _cr;
+        set => _cr = ReplaceInvalidText(value);
+    }
+
+    public bool HasCR => _cr != null;
+
+    public string? Name
+    {
+        get => _name;
+        set => _name = ReplaceInvalidText(value);
+    }
+
+    public bool HasName => _name != null;
+
+    public string? BPNumber
+    {
+        get => _bpNum;
+        set => _bpNum = ReplaceInvalidText(value);
+    }
+
+    public bool HasBPNum => _bpNum != null;
+
+    private string? ReplaceInvalidText(string? value)
+    {
+        if (logger != null)
+        {
+            logger.LogProcessingInfo($"Replacing invalid text in {value ?? ""}");
+        }
+
+        value = value?.Replace("&", "&amp;");
+        value = value?.Replace("<", "&lt;");
+        value = value?.Replace(">", "&gt;");
+        if (logger != null)
+        {
+            logger.LogProcessingInfo($"Replaced text resulted in: {value}");
+        }
+
+        return value;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name ?? ""} {Internet ?? ""} {Publication ?? ""} " +
+               $"{Resume ?? ""} {Title ?? ""} {Index ?? ""} {IndexBis ?? ""} " +
+               $"{No ?? ""} {CR ?? ""} {BPNumber ?? ""} {SBandSEG ?? ""}";
+    }
+
+    public string ToXML()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"<idno type=\"bp\">{BPNumber}</idno>\n");
+        if (_index != null) sb.Append($"<seg type=\"original\" subtype=\"index\" resp=\"#BP\">{_index}</seg>\n");
+        if (IndexBis != null) sb.Append($"<seg type=\"original\" subtype=\"indexBis\" resp=\"#BP\">{IndexBis}</seg>\n");
+        if (_title != null) sb.Append($"<seg type=\"original\" subtype=\"titre\" resp=\"#BP\">{_title}</seg>\n");
+        if (SBandSEG != null) sb.Append($"<seg type=\"original\" subtype=\"SBandSeg\" resp=\"#BP\">{SBandSEG}</seg>\n");
+        if (No != null) sb.Append($"<seg type=\"original\" subtype=\"No\" resp=\"#BP\">{No}</seg>\n");
+        if (annee != null) sb.Append($"<seg type=\"original\" subtype=\"annee\" resp=\"#BP\">{annee}</seg>\n");
+        if (Publication != null)
+            sb.Append($"<seg type=\"original\" subtype=\"publication\" resp=\"#BP\">{Publication}</seg>\n");
+        if (_resume != null) sb.Append($"<seg type=\"original\" subtype=\"resume\" resp=\"#BP\">{_resume}</seg>\n");
+        if (CR != null) sb.Append($"<seg type=\"original\" subtype=\"cr\" resp=\"#BP\">{CR}</seg>\n");
+        if (Name != null) sb.Append($"<seg type=\"original\" subtype=\"nom\" resp=\"#BP\">{Name}</seg>\n");
+        if (_internet != null) sb.Append($"<ptr target=\"{_internet}\"\n");
+
+        return sb.ToString();
+    }
+}
---
+++ BPRemovingDuplicateIdnos/Program.cs
@@ -0,0 +1,148 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using DefaultNamespace;
+
+Console.WriteLine("Staring program");
+
+Console.WriteLine("Setting xml path");
+var xmlFilePath = SetXMLFilepath();
+
+Console.WriteLine("Creating Logger");
+var logger = new Logger();
+
+Console.WriteLine("Gathering XML files");
+var xmlFileGatherer = new XMLEntryGatherer(xmlFilePath, logger);
+var entries = xmlFileGatherer.GatherEntries();
+var matchingEntries = FindMatchingBPEntries(entries);
+
+var comparedMatches = CompareMatchingEntries(matchingEntries);
+
+foreach (var entry in comparedMatches)
+{
+    Console.WriteLine($"Found a troubling match between A: {entry.LevelA.PNNumber} && {entry.LevelM.PNNumber}");
+}
+
+//TODO process what happens once the match has been found.
+
+
+static List<(XMLDataEntry LevelA, XMLDataEntry LevelM)> CompareMatchingEntries(
+    List<(XMLDataEntry m1, XMLDataEntry m2)> matchingEntries)
+{
+    var entriesWeAreConcernedWith = new List<(XMLDataEntry LevelA, XMLDataEntry LevelM)>();
+
+    foreach (var entry in matchingEntries)
+    {
+        XMLDataEntry? a = null;
+        XMLDataEntry? m = null;
+        
+        if(entry.m1.TitleLevel == "a") a = entry.m1;
+        else if (entry.m2.TitleLevel == "a") a = entry.m2;
+        
+        if(entry.m1.TitleLevel == "m") m = entry.m1;
+        else if (entry.m2.TitleLevel == "m") m = entry.m2;
+
+        if (a != null && m != null)
+        {
+            entriesWeAreConcernedWith.Add(new ValueTuple<XMLDataEntry, XMLDataEntry>(a,m));
+        }
+    }
+    
+    return entriesWeAreConcernedWith;
+}
+
+static List<(XMLDataEntry m1, XMLDataEntry m2)> FindMatchingBPEntries(List<XMLDataEntry> xmlDataEntries)
+{
+    var matches = new List<(XMLDataEntry m1, XMLDataEntry m2)>();
+    xmlDataEntries.Sort(delegate(XMLDataEntry x, XMLDataEntry y)
+                  {
+                      if(BPNumbAsInt(x) > BPNumbAsInt(y)) return 1;
+                      if (BPNumbAsInt(x) < BPNumbAsInt(y)) return -1;
+                      if (BPNumbAsInt(x) == BPNumbAsInt(y)) return 0;
+                      return 0;
+                  });
+
+    for (int i = 0; i < xmlDataEntries.Count; i++)
+    {
+        if (i + 1 == xmlDataEntries.Count) break;
+        if ((xmlDataEntries[i].HasBPNum && xmlDataEntries[i + 1].HasBPNum)
+            && (xmlDataEntries[i].BPNumber == xmlDataEntries[i + 1].BPNumber))
+        {
+            matches.Add(new ValueTuple<XMLDataEntry, XMLDataEntry>(xmlDataEntries[i],xmlDataEntries[i+1]));
+        }
+    }
+
+    return matches;
+}
+
+static int BPNumbAsInt(XMLDataEntry entry)
+{
+    if (entry.HasBPNum)
+    {
+        if (Int32.TryParse(entry.BPNumber, out var result))
+        {
+            return result;
+        }
+        else
+        {
+            Console.WriteLine($"Error entry {entry.PNNumber} has no BP Number");
+            return int.MaxValue;
+        }
+        
+    }
+    else
+    {
+        Console.WriteLine($"Error entry {entry.PNNumber} has no BP Number");
+        return int.MaxValue;
+    }
+}
+
+static string SetXMLFilepath()
+{
+    var currnetDir = Directory.GetCurrentDirectory();
+    var idpDataDir = SearchForIDPDataDir(currnetDir);
+    var biblioDir = GetBiblioDir(idpDataDir);
+    
+    return biblioDir;
+}
+
+
+static string GetBiblioDir(string? idpDataDir)
+{
+    var dirs = Directory.GetDirectories(idpDataDir);
+    if (dirs.Any(x => x.ToLower().Contains("biblio")))
+    {
+        return dirs.First(x => x.ToLower().Contains("biblio"));
+    }
+    else
+    {
+        throw new DirectoryNotFoundException($"Could not find Biblio directory in: {idpDataDir}");
+    }
+}
+
+static string SearchForIDPDataDir(string startPath)
+{
+    var dirs = Directory.GetDirectories(startPath);
+    if (dirs.Length > 0)
+    {
+        if (dirs.Any(x => x.Contains("idp.data")))
+        {
+            var idpDir = dirs.First(x => x.Contains("idp.data"));
+            
+            return idpDir;
+        } else if (startPath == "C:/" || startPath == "/")
+        {
+            throw new DirectoryNotFoundException("Could not find IDPData directory");
+        }
+        else
+        {
+            return (SearchForIDPDataDir(Directory.GetParent(startPath).FullName)); 
+        }
+    }
+    else
+    {
+        return (SearchForIDPDataDir(Directory.GetParent(startPath).FullName)); 
+    }
+
+    throw new DirectoryNotFoundException("Could not find IDPData directory");
+}
